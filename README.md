e7b0e6c978c68fdcae27f86a0577ef279a1c1918
tegn903 <79765666+tegn903@users.noreply.github.com>
2021-02-27 22:32:30 +0300

    Update and rename README.md to Tegn.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# bytegn
-Bytegn78
---
+++ Tegn.py
@@ -0,0 +1,156 @@
+
+
+
+import time
+print("Sisteme Giriş Yapınız")
+print("╔═══════════════╗")
+print("   「 RedHACK 💋    ")
+print("╚═══════════════╝")
+time.sleep(3)
+print("A)1 B)9 C)9 D)7 ")
+time.sleep(2)
+a=int(input("Cevabınız="))
+time.sleep(2)
+print("Giriş Yapılıyor...")
+time.sleep(4)
+if a==7 :print("Sistem Giriş Başarılı Welcome ByTegn")
+else :print("Sistem Giriş Başarısız") 
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+time.sleep(1)
+print("░░░░░▄▄▀▀▀▀▀▀▀▀▀▄▄░░░░░")
+time.sleep(1)
+print("░░░░█░░░░░░░░░░░░░█░░░░")
+time.sleep(1)
+print("░░░█░░░░░░░░░░▄▄▄░░█░░░")
+time.sleep(1)
+print("░░░█░░▄▄▄░░▄░░███░░█░░░")
+time.sleep(1)
+print("░░░▄█░▄░░░▀▀▀░░░▄░█▄░░░")
+time.sleep(1)
+print("░░░█░░▀█▀█▀█▀█▀█▀░░█░░░")
+time.sleep(1)
+print("░░░▄██▄▄▀▀▀▀▀▀▀▄▄██▄░░░")
+time.sleep(1)
+print("░▄█░█▀▀█▀▀▀█▀▀▀█▀▀█░█▄░")
+time.sleep(1)
+print("░░░░░░░░░░░░░░░░░░░░░░░")
+time.sleep(1)
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print("Merhaba.")
+isim = input('İsminizi Girin : ')
+print("Merhaba "+isim)
+
+print("VÜCUT KİTLE ENDEKSİ HESAPLAMA")
+boy = float(input("Boy (m):"))
+kilo = int(input("Kilo (kg):"))
+ 
+endeks  = kilo/(boy*boy)
+ 
+if endeks <=18:
+    print("\n zayıf VKİ:{}".format(endeks))
+elif endeks > 18 and endeks <=25 :
+    print("\n kilolu VKİ:{}".format(endeks))
+elif endeks > 25 and endeks <=30:
+    print("\n obez VKİ:{}".format(endeks))
+elif endeks > 30:
+    print("\n ciddi obez VKİ:{}".format(endeks))
+
+print("Aramıza Hoşgeldin")
+time.sleep(0.075)
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from time import sleep
+
+metin = """
+Merhaba dostum...
+Bu ilginç görünüyor olabilir fakat, tek tek ekrana dökülen yazıları görüyorsun.
+Ya ne saçmalıyorum ben, sadece uzun bir yazı olmasını istedim. :)
+Ve oldu da!...
+"""
+
+for x in metin: # Metindeki her öğeye "x" diyoruz.
+    print(x,end="",flush=True) # x'i ekrana basıyoruz. Ve yeniliyoruz.
+    sleep(0.075) # Azıcık bekliyoruz...
+print() # En sonunda ise bir satır boşluk ekliyoruz.
+
+time.sleep(3)
+print(" Code:1 Devam Et ")
+time.sleep(2)
+a=int(input("Code="))
+time.sleep(2)
+print("Giriş Yapılıyor...")
+time.sleep(4)
+if a==1 :print("Sistem Giriş Başarılı Welcome")
+else :print("Sistem Giriş Başarısız") 
+
+
+
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from time import sleep
+
+metin = """
+Merhaba dostum...
+Hacker Olmak İçin Sabırlı Olmak Gerek Bu Yüzden Başarıya Giden Yolda Kendin Ol   :)
+...
+"""
+
+for x in metin: # Metindeki her  "x" diyoruz.
+    print(x,end="",flush=True) # x'i
+sleep(0.075) 
